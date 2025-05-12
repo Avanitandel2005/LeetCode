a8dbff77737618ee17c8d29c03606f44d54b6988
Avanitandel2005 <avanitandel01@gmail.com>
2025-05-12 09:04:27 +0530

    Time: 0 ms (100%), Space: 40.4 MB (50.58%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int climbStairs(int n) {
+        int a = 0;
+        int b = 1;
+        if(n<=2){
+            return n;
+        }
+        for(int i=1;i<=n;i++){
+            int temp = b;
+            b=b+a;
+            a=temp;
+
+        }
+        return b;
+        
+        
+    }
+}
