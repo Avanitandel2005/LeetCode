731c7710c55a633cb82bc340738e4e2040fdfcda
Avanitandel2005 <avanitandel01@gmail.com>
2025-05-14 18:31:43 +0530

    Time: 0 ms (100%), Space: 42.8 MB (72.52%) - LeetHub

---
+++ 1791-richest-customer-wealth/1791-richest-customer-wealth.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        
+        int max =Integer.MIN_VALUE;
+        for(int row=0;row<accounts.length;row++){
+            int rowsum = 0;
+            for(int col=0; col<accounts[row].length;col++){
+                rowsum += accounts[row][col];
+            }
+            if(rowsum > max){
+                max=rowsum;
+            }
+        }
+        return max;
+        
+    }
+}
