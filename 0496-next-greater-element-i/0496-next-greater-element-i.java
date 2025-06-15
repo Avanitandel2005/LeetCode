dab46d9673600e0eeec8033ccbd1b3a0219b2e25
Avanitandel2005 <avanitandel01@gmail.com>
2025-06-15 22:41:16 +0530

    Time: 2 ms (99.09%), Space: 42.8 MB (99.21%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,6 +1,5 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        Stack <Integer> ele = new Stack<>();
         int result[] = new int[nums1.length];
         for(int i=0;i<nums1.length;i++){
             int num = nums1[i];
